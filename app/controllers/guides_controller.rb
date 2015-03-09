a2da601c61380e8657804e95361c9337914d811e
kenrett <kenrettberg@gmail.com>
2015-03-08 20:26:40 -0700

    Added guides model and controller (new, update, index, create)

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,6 +1,32 @@
 class GuidesController < ApplicationController
 
+  def index
+    @guides = Guide.all
+  end
+
   def new
     @guide = Guide.new
+  end
+
+  def create
+    redirect_to root_path
+  end
+
+  def update
+    @guide = Guide.find(guide_params)
+
+    if @guide.update_attributes(guide_params)
+        flash[:notice] = "Success"
+        redirect_to guides_path(@guide)
+      else
+        flash.now[:alert] = @guide.errors.full_messages.first
+        render :edit
+    end
+  end
+
+  private
+
+  def guide_params
+    params.require(:guides).permit(:title, :description, :price)
   end
 end
--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,3 +1,4 @@
 class Guide < ActiveRecord::Base
+  validates_presence_of :title, :description, :price
 
 end
