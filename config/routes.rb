3c238e8741941efda2c717cd15784b8792e7c7d2
kenrett <kenrettberg@gmail.com>
2015-03-29 19:54:46 -0700

    Added the rest of the resources for guides

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   root 'home#index'
 
-  resources 'guides', :only => [:new, :create, :index]
+  devise_for :users
+  resources 'guides'
 end
