a0c9944674f50c7281803e104c477cd13ef1a557
kenrett <kenrettberg@gmail.com>
2015-04-02 21:55:31 -0700

    Guides belong to books and added other controller actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,6 +20,30 @@ class BooksController < ApplicationController
     end
   end
 
+  def edit
+    @book = Book.find(params[:id])
+    unauthorized! if cannot? :edit, @book
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @guide.update_attributes(guide_params)
+      flash[:notice] = 'Success'
+      redirect_to book_path(@book)
+    else
+      flash.now[:alert] = @book.errors.full_messages.first
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to :action => 'index'
+  end
+
   private
 
   def book_params
