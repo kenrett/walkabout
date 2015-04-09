ca032dd4f9e2880dfed580718d5fc899e0c3da0d
kenrett <kenrettberg@gmail.com>
2015-04-08 21:32:04 -0700

    WIP - Database_cleaner is wiping out entire db, not just the spec created things...

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,40 +12,34 @@ RSpec.configure do |config|
   # RSpec 3 does not automatically do this anymore...
   config.infer_spec_type_from_file_location!
 
+  config.use_transactional_fixtures = false
+
   # config.include Rack::Test::Methods
   config.include Devise::TestHelpers, :type => :controller
 
   config.expect_with :rspec do |expectations|
-
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
   config.before(:suite) do
-    DatabaseCleaner.clean_with :truncation
-  end
-
-  config.after(:suite) do
-    FileUtils.rm_r Rails.root.join("public/system") if File.directory?(Rails.root.join("public/system"))
-    FileUtils.mkdir_p Rails.root.join("public/system") unless File.directory?(Rails.root.join("public/system"))
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
+    FactoryGirl.reload
   end
 
-  config.before(:each, type: :feature) do
+  config.before(:each, :js => true) do
     DatabaseCleaner.strategy = :truncation
+    FactoryGirl.reload
   end
 
   config.before(:each) do
     DatabaseCleaner.start
   end
 
-  config.after(:each) do
+  config.append_after(:each) do
     DatabaseCleaner.clean
   end
 end
